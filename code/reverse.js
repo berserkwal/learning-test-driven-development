caac14222091c2bb25ca51e7492db9eee48cb4be
Vikas Kharkwal <kharkalvikas@gmail.com>
2021-07-24 18:03:48 +0530

    made the function cleaner

--- code/reverse.js
+++ code/reverse.js
@@ -1,10 +1,10 @@
 function reverseString(string) {
-	const reverse = [];
-
-	for (let i = string.length - 1; i >= 0; i--) {
-		reverse.push(string[i]);
-	}
-	return reverse.join("");
+	return string
+		.split("")
+		.sort(() => {
+			return -1;
+		})
+		.join("");
 }
 
 module.exports = reverseString;
